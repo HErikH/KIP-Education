97113aaad735909276c3b0386e0050c6107bdc6f
HErikH <erikharutyunyan398@gmail.com>
2025-11-06 20:25:43 +0400

    fix(api): delete docx and pptx support for upload

--- classroom_platform/backend/helpers/constants/whiteboard.js
+++ classroom_platform/backend/helpers/constants/whiteboard.js
@@ -1,8 +1,6 @@
-export const ALLOWED_EXTENSIONS_TYPES = /jpeg|jpg|png|gif|pdf|docx|pptx/;
+export const ALLOWED_EXTENSIONS_TYPES = /jpeg|jpg|png|gif|pdf/;
 
 export const FILE_TYPES = {
   img: "image/",
-  pdf: "application/pdf",
-  doc: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-  pptx: "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+  pdf: "application/pdf"
 };
